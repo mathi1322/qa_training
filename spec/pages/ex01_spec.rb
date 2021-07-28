10fe5e26235bc0685520c8bd51f9351f547b55cf
mathi1322 <mathivanan@drylogics.com>
2021-07-28 15:17:11 +0530

    Login process

---
+++ spec/pages/ex01_spec.rb
@@ -0,0 +1,14 @@
+require 'byebug'
+require_relative '../../pages/LoginHelper.rb'
+describe 'Login Process' do
+  context "when user log in" do
+    it 'user can sign in', type: :feature do
+      dashboard_url = 'https://explorer.stage.lfmprod.in/#dashboard/start'
+      sign_in = LoginPage.new
+      sign_in.load
+      sleep 20
+      sign_in.login('turneruser@turner.com', 'turnertest')
+      expect(sign_in.current_url).to eq(dashboard_url)
+    end
+  end
+end
