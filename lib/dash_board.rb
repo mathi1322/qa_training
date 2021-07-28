c60a3dbd691e5f12573e2901085df0c968998854
mathi1322 <mathivanan@drylogics.com>
2021-07-28 15:51:23 +0530

    deleted unused files

--- lib/dash_board.rb
+++
@@ -1,8 +0,0 @@
-class Dashboard < SitePrism::Page
-  element :menu, "div.menu-grid"
-
-  def click_item(name)
-    item = menu.find_all("div.menu-item").find { |x| x.find("div.item-label").text == name }
-    item.click
-  end
-end
