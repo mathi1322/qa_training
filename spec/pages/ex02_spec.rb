143027fbff9a5b6004d87973b176b1b6da347ebc
mathi1322 <mathivanan@drylogics.com>
2021-08-02 02:24:19 +0530

    ex02 added

---
+++ spec/pages/ex02_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../pages/LoginHelper.rb'
+require_relative '../../pages/Dashboard.rb'
+
+describe 'Startscreen menu items' do
+  context "selecting a menu item" do
+    it 'do select a menu', type: :feature do
+      dashboard_url = 'https://explorer.stage.lfmprod.in/#dashboard/start'
+      sign_in = LoginHelper.new
+      sign_in.load
+      sleep 20
+      sign_in.login('turneruser@turner.com', 'turnertest')
+      expect(sign_in.current_url).to eq(dashboard_url)
+			select_menu = Dashboard.new
+      select_menu.click_item('Brands')  
+      sleep 20
+    end
+  end
+end
