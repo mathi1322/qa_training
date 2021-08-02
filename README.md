56d1848c5b77d74ed47ba26717a697f0a5072384
mathi1322 <mathivanan@drylogics.com>
2021-08-02 20:06:36 +0530

    Added tag and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # qa_training
+ex05:
+  1. Login to dashboard using a config file
+  2. Clicking 'Brands' to navigate Brand listing page.
--- spec/pages/ex05_spec.rb
+++ spec/pages/ex05_spec.rb
@@ -2,16 +2,17 @@ require '/home/mathivanan/git/qa_training/pages/LoginPage.rb'
 require '/home/mathivanan/git/qa_training/lib/config.rb'
 require '/home/mathivanan/git/qa_training/pages/Dashboard.rb'
 
-describe "the signin process", type: :feature do
-  it "signs me in" do
-    LoginPage.login
-    sleep 20
-    expect(page.current_url).to eq(Config.get_value(:dashboard_url))
-  end 
-  
-  it "selecting menu item" do
-    sign = Dashboard.new
-    sign.click_item('Brands')
-    sleep 20
-  end 
+describe "Login process", type: :feature do
+  context "using a config file" do
+    it "signs me in", case: 'ex05_1' do
+      LoginPage.login
+      sleep 20
+      expect(page.current_url).to eq(Config.get_value(:dashboard_url))
+    end 
+    it "selecting menu item", case: 'ex05_2' do
+      sign = Dashboard.new
+      sign.click_item('Brands')
+      sleep 20
+    end 
+  end  
 end
